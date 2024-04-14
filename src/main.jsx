00f4a9bd8ba75784ea2668d8e43e0ff7705bd1c1
Trilok Rana <58104039+Trilokrana@users.noreply.github.com>
2024-04-15 01:52:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { StateContextProvider } from './Context/index.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { StateContextProvider } from "./Context/index.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StateContextProvider>
     <App />
-  </StateContextProvider>,
-)
+    <ToastContainer
+      theme="light"
+      position="bottom-right"
+      autoClose={2000}
+      hideProgressBar
+      newestOnTop
+      closeOnClick
+      rtl
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+  </StateContextProvider>
+);
